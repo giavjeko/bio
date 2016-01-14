3bc2bf063a5e12bba379de152c5d8597df8904c0
giavjeko <vjeko@entropia.hr>
2016-01-14 22:08:03 +0100

    File IO.

--- main.c
+++ main.c
@@ -32,9 +32,9 @@ void compute_lcp(Wtree* wtree, int* dst, int len) {
   List* list = list_construct();
   Element* element;
 
-  dst[0] = -1;
-  dst[len - 1] = -1;
-  for (i = 1; i < len - 1; i++) {
+  dst[1] = -1;
+  dst[len + 1] = -1;
+  for (i = 2; i <= len; i++) {
     dst[i] = -2;
   }
 
@@ -43,31 +43,58 @@ void compute_lcp(Wtree* wtree, int* dst, int len) {
     queue_pop(queue, &begin, &end, &l);
     list = wtree_get_intervals(wtree, begin, end);
     for (element = list->head; element; element=element->next) {
-      if (dst[element->end] == -2) {
+      if (dst[element->end + 1] == -2) {
         queue_push(queue, element->begin, element->end, l + 1);
-        dst[element->end] = l;
+        dst[element->end + 1] = l;
       }
     }
   }
-
 }
 
-int main() {
-  char* input = "el_anele_lepanelen$";
-  int len = strlen(input);
-  char* alphabet = generate_alphabet(input, len);
-  char* bwt = (char*)malloc((strlen(input) + 1) * sizeof(char));
-  bwt_transform(input, bwt, strlen(input));
-  Wtree* wtree = generate_wtree(bwt, strlen(input), alphabet, strlen(alphabet));
-  printf("%s\n%s\n%s\n\n%s\n", input, alphabet, bwt, wtree_tostring(wtree));
+void process(char* string, int* result, int len) {
+  char* bwt = (char*)malloc((len + 1) * sizeof(char));
+  char* alphabet = generate_alphabet(string, len);
+  bwt_transform(string, bwt, len);
+  Wtree* wtree = generate_wtree(bwt, len, alphabet, strlen(alphabet));
+  compute_lcp(wtree, result, len);
+  free(bwt);
+  free(alphabet);
+  free(wtree);
+}
 
-  int* lcp = (int*)malloc(len * sizeof(int));
-  compute_lcp(wtree, lcp, len);
-  printf("lcp:\n");
-  int i;
-  for (i = 0; i < len; i++) {
-    printf("%d ", lcp[i]);
+void input(char* filename, char** dst, int* len) {
+  FILE* file = fopen(filename, "r");
+  fseek(file, 0, SEEK_END);
+  *len = ftell(file) - 1;
+  fseek(file, 0, SEEK_SET);
+  *dst = (char*)malloc((*len + 1) * sizeof(dst));
+  fscanf(file, "%s", *dst);
+  fclose(file);
+}
+
+void output(char* filename, int* data, int len) {
+  FILE* file = fopen(filename, "w");
+  for (int i = 1; i <= len + 1; i++) {
+    fprintf(file, "%d\n", data[i]);
   }
-  printf("\n");
+  fclose(file);
+}
+
+int main(int argc, char** argv) {
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s input_file_path output_file_path\n", *argv);
+    exit(1);
+  }
+  char* input_path = *(argv + 1);
+  char* output_path = *(argv + 2);
+  int len;
+  char* string;
+
+  input(input_path, &string, &len);
+  int* result = (int*)malloc((len + 2) * sizeof(int));
+  process(string, result, len);
+  output(output_path, result, len);
+  free(result);
+
   return 0;
 }
