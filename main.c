c97173504a88b93b34a3ce97660b7f577016d96a
giavjeko <vjeko@entropia.hr>
2016-01-14 20:35:55 +0100

    Main style improvement.

--- main.c
+++ main.c
@@ -5,66 +5,69 @@
 #include "set.c"
 #include "queue.c"
 
+// Return alphabet for given string
 char* generate_alphabet(char* string, int len) {
-  Set* set = set_construct();
   int i;
+  Set* set = set_construct();
   for (i = 0; i < len; i++) {
     set_push(set, string[i]);
   }
   return set_tostring(set);
 }
 
+// Return Wavelet tree for given string and alphabet
 Wtree* generate_wtree(char* string, int string_len, char* alphabet, int alphabet_len) {
-  Wtree* wtree = wtree_construct(alphabet, alphabet_len);
   int i;
+  Wtree* wtree = wtree_construct(alphabet, alphabet_len);
   for (i = 0; i < string_len; i++) {
     wtree_push(wtree, string[i]);
   }
   return wtree;
 }
 
-int* compute_LCP(Wtree* wtree) {
-  int i, j, l;
-  int n = wtree->root->bitvector->length;
-  int* LCP = (int*)malloc((n+1)*sizeof(int));
-  int ii = 0;
+// Compute lcp for given Wavelet tree with data
+void compute_lcp(Wtree* wtree, int* dst, int len) {
+  int i, begin, end, l;
   Queue* queue = queue_construct();
   List* list = list_construct();
   Element* element;
 
-  for (ii = 1; ii <= n+1; ii++) {
-    if (ii == 1 || ii == n+1) LCP[ii] = -1;
-    else LCP[ii] = -2;
+  dst[0] = -1;
+  dst[len - 1] = -1;
+  for (i = 1; i < len - 1; i++) {
+    dst[i] = -2;
   }
 
-  queue_push(queue,1,n,0);
+  queue_push(queue, 1, len, 0);
   while(!queue_empty(queue)) {
-    queue_pop(queue,&i,&j,&l);
-    list = wtree_get_intervals(wtree,i,j);
+    queue_pop(queue, &begin, &end, &l);
+    list = wtree_get_intervals(wtree, begin, end);
     for (element = list->head; element; element=element->next) {
-      if (LCP[element->end+1] == -2) {
-        queue_push(queue,element->begin,element->end,l+1);
-        LCP[element->end+1] = l;
+      if (dst[element->end] == -2) {
+        queue_push(queue, element->begin, element->end, l + 1);
+        dst[element->end] = l;
       }
     }
   }
 
-  printf("LCP:\n");
-  for (ii = 1; ii <= n+1; ii++) {
-    printf("%d ",LCP[ii]);
-  }
-  printf("\n");
 }
 
-int main () {
+int main() {
   char* input = "el_anele_lepanelen$";
-  char* alphabet = generate_alphabet(input, strlen(input));
+  int len = strlen(input);
+  char* alphabet = generate_alphabet(input, len);
   char* bwt = (char*)malloc((strlen(input) + 1) * sizeof(char));
   bwt_transform(input, bwt, strlen(input));
   Wtree* wtree = generate_wtree(bwt, strlen(input), alphabet, strlen(alphabet));
   printf("%s\n%s\n%s\n\n%s\n", input, alphabet, bwt, wtree_tostring(wtree));
 
-  compute_LCP(wtree);
-  system("PAUSE");
+  int* lcp = (int*)malloc(len * sizeof(int));
+  compute_lcp(wtree, lcp, len);
+  printf("lcp:\n");
+  int i;
+  for (i = 0; i < len; i++) {
+    printf("%d ", lcp[i]);
+  }
+  printf("\n");
   return 0;
 }
